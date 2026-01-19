619f375e9763301ea8d50d6ac20657773a95c8e0
Soham Mule <sohammule09@gmail.com>
2026-01-19 17:10:30 +0000

    my-orders features added

--- frontend/src/pages/managelistings.jsx
+++ frontend/src/pages/managelistings.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
 import { LoaderIcon, toast } from "react-hot-toast"
+import { Upload, X } from "lucide-react"
+
 
 const ManageListings = () => {
   const { id } = useParams()
@@ -38,10 +40,11 @@ const ManageListings = () => {
   }
 
   const handleImageUpload = async (e) => {
+    console.log(e.target.files)
     const files = Array.from(e.target.files)
     if (!files.length) return
     if (files.length + formData.images.length > 5) return toast.error("You can add upto 5 images")
-    SetFormData((prev) => ({ ...prev, images: [...prev.images], ...files }))
+    SetFormData((prev) => ({ ...prev, images: [...prev.images, ...files] }))
   }
   const removeImage = (indextoRemove) => {
     SetFormData((prev) => ({ ...prev, images: prev.images.filter((_, i) => i !== indextoRemove) }))
@@ -92,8 +95,74 @@ const ManageListings = () => {
           <Section title="Basic Information" >
             <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
               <InputField label="Listing Title *" value={formData.title} placeholder="e.g., Premium Travel Instagram Account" onChange={(v) => handleInputChange('title', v)} required={true} />
+              <SelectField label={'Platform *'} options={platforms} value={formData.platform} onChange={(v) => handleInputChange('platform', v)}
+                required={true}
+              />
+              <InputField label="Username/Handle *" value={formData.username} placeholder="@username" onChange={(v) => handleInputChange('username', v)} required={true} />
+
+              <SelectField label={'Niche/Category *'} options={niche} value={formData.niche} onChange={(v) => handleInputChange('niche', v)}
+                required={true}
+              />
+
             </div>
           </Section>
+          {/* Metrics */}
+          <Section title='Account Metrics'>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mb-6'>
+              <InputField label={'Followers Count *'} type='number' min={0} value={formData.followers_count} placeholder={'10000'} onChange={(v) => handleInputChange('followers_count', v)} required={true} />
+
+              <InputField label={'Engagement Rate (%)'} type='number' min={0} max={100} value={formData.engagement_rate} placeholder={'4'} onChange={(v) => handleInputChange('engagement_rate', v)} required={true} />
+
+              <InputField label={'Montly Views/Impression'} type='number' min={0} value={formData.monthly_views} placeholder={'100000'} onChange={(v) => handleInputChange('monthly_views', v)} required={true} />
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mb-6'>
+              <InputField label={'Primary Audience Country'} type='number' min={0} value={formData.country} placeholder={'United States'} onChange={(v) => handleInputChange('country', v)} required={true} />
+
+              <SelectField label={'Primary Audiency Age Range *'} options={ageRange} value={formData.age_range} onChange={(v) => handleInputChange('age_range', v)}
+
+              />
+              <div className='space-y-3'>
+                <CheckBoxField label={'Account is verified on the platform'} checked={formData.verfied} onChange={(v) => handleInputChange('verified', v)} />
+                <CheckBoxField label={'Account is monetized'} checked={formData.monetized} onChange={(v) => handleInputChange('verified', v)} />
+              </div>
+              {/* Pricing */}
+            </div>
+          </Section>
+          <Section title={'Pricing and Description'}>
+            <InputField label={'Asking Price (USD) *'} type='number' value={formData.price} placeholder={'2500.00'} onChange={(v) => handleInputChange('price', v)} required={true} />
+
+            <TextAreaField label={'Description *'} value={formData.description} onChange={(v) => handleInputChange('description', v)} required={true} />
+          </Section>
+          {/* Image Upload */}
+          <Section title={'Screenshots and Proof'}>
+            <div className='border-2 border-dashed border-gray-300 rounded-lg p-6 text-center'>
+              <input type='file' id='images' multiple accept='image/*' onChange={handleImageUpload} className='hidden' />
+              <Upload className='w-12 h-12 text-gray-400 mx-auto mb-4' />
+              <label htmlFor="images" className='px-4 py-2 border border-gray300 rounded-lg hover:bg-gray-50 cursor-pointer'>Choose Files</label>
+              <p className='text-sm text-gray-500 mt-2'>Upload screenshots or proof of account analytics</p>
+            </div>
+            {formData.images.length > 0 && (
+              <div className='grid grid-cols-2 md:grid-cols-4 gap-4 mt-4'>
+                {formData.images.map((img, index) => (
+                  <div key={index} className='relative'>
+                    <img src={typeof img === 'string' ? img : URL.createObjectURL(img)} alt={`image ${index + 1}`} className='w-full h-24 object-cover rounded-lg' />
+                    <button type='button' onClick={() => removeImage(index)}>
+                      <X className='absolute  -top-2 -right-2 size-6 bg-red-600 text-white rounded-full hover:bg-red-700' />
+                    </button>
+                  </div>
+                ))}
+              </div>
+            )}
+          </Section>
+          <div className='flex justify-end gap-3 text-sm'>
+            <button type='button' onClick={() => navigate(-1)} className='px-6 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors'>
+              Cancel
+            </button>
+            <button type='submit' className='px-6 py-1 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition-colors'>
+            {isEditing ? "Update-Listing" : 'Create-Listing'}
+            </button>
+          </div>
         </form>
       </div>
     </div>
@@ -120,15 +189,38 @@ const InputField = ({ label, value, onChange, placeholder, type = 'text', requir
 
 const SelectField = ({ label, options, value, onChange, required = false }) => {
   return (
-  <div>
-    <label className='block text-sm font-medium text-gray-700 mb-2'>{label}</label>
-  <select value={value} onChange={(e)=>onChange(e.target.value)} className='w-full px-3 py-1.5 text-gray-600 border rounded-md focus:outline-none focus:ring-indigo-500 border-gray-300'>
-
-  </select>
-  </div>
+    <div>
+      <label className='block text-sm font-medium text-gray-700 mb-2'>{label}</label>
+      <select value={value} onChange={(e) => onChange(e.target.value)} className='w-full px-3 py-1.5 text-gray-600 border rounded-md focus:outline-none focus:ring-indigo-500 border-gray-300' required={required}>
+        <option value={""}>Select...</option>
+        {options.map((opt) => {
+          return (
+            <option value={opt} key={opt}>{opt}</option>
+          )
+        })}
+      </select>
+    </div>
 
   )
 
+}
+
+const CheckBoxField = ({ label, checked, onChange, required = false }) => {
+  return (
+    <label className='flex items-center space-x-2 cursor-pointer'>
+      <input type="checkbox" checked={checked} onChange={(e) => onChange(e.target.value)} className='size-4' required={required} />
+      <span className='text-smn text-gray-700'>{label}</span>
+    </label>
+  )
+}
+
+const TextAreaField = ({ label, value, onChange, required = false }) => {
+  return (
+    <div>
+      <label className='block text-sm font-medium text-gray-700 mb-2'>{label}</label>
+      <textarea rows={5} value={value} onChange={(e) => onChange(e.target.value)} className='w-full px-3 py-1.5 text-gray-600 border rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 border-gray-300' required={required} />
+    </div>
+  )
 }
 
 export default ManageListings
