1484364f28465b562aa8a89c2aba0bde26630abe
Soham Mule <sohammule09@gmail.com>
2026-01-22 18:08:52 +0000

    added suffix

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
-import prisma from "../configs/prisma";
+import prisma from "../configs/prisma.js";
+
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "Market-Place" });
